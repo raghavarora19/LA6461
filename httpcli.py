d1a8b7681d00f73fd956ee7ab6d660d9c29eb742
Raghav Arora <raghavarora@Raghavs-MacBook-Pro.local>
2018-09-30 22:06:54 -0400

    Redirection

---
+++ httpcli.py
@@ -0,0 +1,95 @@
+import socket
+import argparse
+
+
+def get(verbose,header,optional,URL):
+    request = "GET / HTTP/1.0\r\nHost: "+ URL + "\r\n\r\n"
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("www.google.com", 80))
+    s.sendall(request.encode())
+    result = s.recv(1024)
+    abc = result.decode()
+    body = abc.split('\r\n\r\n')  # For Verbose
+    if verbose == True:
+        print('Output Get with Verbose : \n', result.decode())
+    else:
+        print('Output Get w/o Verbose : \n ', body[1])
+    s.close()
+
+
+def post(verbos,header,data,file,optional,URL):
+ host = "www.httpbin.org"
+ port = 80
+ if(data == None):
+     data = ""
+ print(header)
+ head= ""
+ for i in header:
+    # print(i)
+     head= head + i + "\r\n"
+
+ #head='\r\n'.join(header)
+ print(head)
+
+ body = '' + data + ''
+ lbody=len(body)
+
+ headers = """\
+POST /post HTTP/1.1\r
+Content-Type: application/json\r
+Content-Length: """ + str(lbody) + """\r
+Host: www.httpbin.org/post\r
+Connection: close\r"""+"""\n"""+head+"""\r
+\r\n """
+
+ body_bytes = body.encode('ascii')
+ header_bytes = headers.format(
+    content_type="application/json",
+    content_length=len(body_bytes),
+    host=str(host) + ":" + str(port)
+ ).encode('iso-8859-1')
+
+ payload = header_bytes + body_bytes
+ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ s.connect(("www.httpbin.org", 80))
+ s.sendall(payload)
+ payload = s.recv(1024)
+ abc = payload.decode()
+ body = abc.split('\r\n\r\n')  # For Verbose
+ if verbos == True:
+        print('\n Output:\n', payload.decode())
+ else:
+        print('\n Output:\n', body[1])
+ s.close()
+
+
+
+def main():
+
+    argParser = argparse.ArgumentParser()
+
+    argParser.add_argument('req_type', type=str, help="GET/POST")
+    argParser.add_argument('-v', "--verbose", action='store_true', help="Increase output ")
+    argParser.add_argument('-s', '--header', action='append', help="Headers to HTTP Request with the format")
+    argParser.add_argument('-d', '--data', action='store', help="An inline data to the body HTTP POST request")
+    argParser.add_argument('-f', '--file', action='store', help="Use -f filename")
+    argParser.add_argument('-o', '--optional', action='store', help="Write Body of Response to File" )
+    argParser.add_argument('URL', type=str, help='Enter the URL ')
+    args = argParser.parse_args()
+    #print("Result : ", args.verbose,args.header,args.data,args.file,args.optional,args.URL)
+    if args.req_type == 'get' and 'GET':
+        if args.header == None:
+            nhead=" "
+            get(args.verbose, nhead, args.optional, args.URL)
+        else : get(args.verbose, args.header, args.optional, args.URL)
+
+    elif args.req_type == 'post' and 'POST':
+        if args.header == None :
+            nhead=" "
+            post(args.verbose, nhead, args.data, args.file, args.optional, args.URL)
+        else : post(args.verbose, args.header, args.data, args.file, args.optional, args.URL)
+
+
+
+
+main()
--- httpclient.py
+++ httpclient.py
@@ -2,70 +2,115 @@ import socket
 import argparse
 
 
-def get(verbose,header,optional,URL):
-    request = "GET / HTTP/1.0\r\nHost: "+ URL + "\r\n\r\n"
+def get(verbose, header, optional, URL):
+    request = "GET / HTTP/1.0\r\nHost: " + URL + "\r\n\r\n"
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("www.google.com", 80))
+    s.connect(("" + URL + "", 80))
     s.sendall(request.encode())
     result = s.recv(1024)
     abc = result.decode()
-    body = abc.split('\r\n\r\n')  # For Verbose
+    body = abc.split('\r\n\r\n')
+    redirectget(body, False, verbose, header, optional, URL)  # call to redirect function
+    if optional:
+        f1 = open(optional, "w+")
+        f1.write(body[1])
+        if verbose:
+            print('Output Get with Verbose + Body Output to file ' + optional + ': \n ', body[0])
+            exit(0)
+        else:
+            exit(0)
+    # For Verbose
     if verbose == True:
         print('Output Get with Verbose : \n', result.decode())
     else:
         print('Output Get w/o Verbose : \n ', body[1])
+
     s.close()
 
 
-def post(verbos,header,data,file,optional,URL):
- host = "www.httpbin.org"
- port = 80
- if(data == None):
-     data = ""
- print(header)
- head= ""
- for i in header:
-    # print(i)
-     head= head + str(i) + " \r\n"
+def post(verbos, header, data, file, optional, URL):
+    if (data == None):
+        data = ""
+    if file:
+        f1 = open(file, "r")
+        read_file = f1.read()
+        body = '' + data + ' ' + read_file + ''
+    else:
+        body = '' + data + ''
 
- #head='\r\n'.join(header)
- print(head)
 
- body = '' + data + ''
- lbody=len(body)
+    host = URL
+    port = 80
+    head = ""
+    for i in header:
+        head = head + str(i) + " \r\n"
 
- headers = """\
-POST /post HTTP/1.1\r
+    lbody = len(body)
+    post=URL.split('/')
+    #print(post[0])
+
+    headers = """\
+POST http://"""+URL+""" HTTP/1.1\r
 Content-Type: application/json\r
 Content-Length: """ + str(lbody) + """\r
-Host: www.httpbin.org/post\r
-Connection: close\r
-\r\n """ + head + """\r\n"""
+Host: """+ URL + """\r
+Connection: close\r""" + """\n""" + head + """\r
+"""
 
- body_bytes = body.encode('ascii')
- header_bytes = headers.format(
-    content_type="application/json",
-    content_length=len(body_bytes),
-    host=str(host) + ":" + str(port)
- ).encode('iso-8859-1')
+    body_bytes = body.encode('ascii')
+    header_bytes = headers.format(
+        content_type="application/json",
+        content_length=len(body_bytes),
+        host=str(host) + ":" + str(port)
+    ).encode('iso-8859-1')
 
- payload = header_bytes + body_bytes
- s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- s.connect(("www.httpbin.org", 80))
- s.sendall(payload)
- payload = s.recv(1024)
- abc = payload.decode()
- body = abc.split('\r\n\r\n')  # For Verbose
- if verbos == True:
-        print('\n Output:\n', payload.decode())
- else:
-        print('\n Output:\n', body[1])
- s.close()
+    payload = header_bytes + body_bytes
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((""+post[0]+"", 80))
+    s.sendall(payload)
+    payload = s.recv(1024)
+    abc = payload.decode()
+    body = abc.split('\r\n\r\n')
+    redirectput(body, True, verbos, header, data, file, optional, URL)  # For Verbose
+    if optional:
+        f2 = open(optional, "w+")
+        f2.write(body[1])
+        if verbos:
+            print('Output Post with Verbose + Body Output to file ' + optional + ': \n ', body[0])
+            exit(0)
+        else:
+            exit(0)
+    if verbos == True:
+        print('\n Output with Verbose:\n', payload.decode())
+    else:
+        print('\n Output w/o Verbose:\n', body[1])
+    s.close()
 
 
+def redirectget(payload, reqtype, verbose, header, optional, URL):
+    payload[0]
+    stat = payload[0].splitlines()
+    status = stat[0].split()
+    status_code = status[1]
+    print(int(status_code))
+    if (int(status_code) >= 300) & (int(status_code) < 400):
+        print("You have been Redirected :")
+        if reqtype == False:
+            get(verbose, header, optional, 'www.google.com')
+            exit(0)
+def redirectput(payload, reqtype, verbose, header, data, file, optional, URL):
+    payload[0]
+    stat = payload[0].splitlines()
+    status = stat[0].split()
+    status_code = status[1]
+    print(int(status_code))
+    if (int(status_code) >= 300) & (int(status_code) < 400):
+        print("You have been Redirected :")
+        if reqtype == True:
+            post(verbose, header, data, file, optional, "www.httpbin.org/post")
+            exit(0)
 
 def main():
-
     argParser = argparse.ArgumentParser()
 
     argParser.add_argument('req_type', type=str, help="GET/POST")
@@ -73,25 +118,27 @@ def main():
     argParser.add_argument('-s', '--header', action='append', help="Headers to HTTP Request with the format")
     argParser.add_argument('-d', '--data', action='store', help="An inline data to the body HTTP POST request")
     argParser.add_argument('-f', '--file', action='store', help="Use -f filename")
-    argParser.add_argument('-o', '--optional', action='store', help="Write Body of Response to File" )
+    argParser.add_argument('-o', '--optional', action='store', help="Write Body of Response to File")
     argParser.add_argument('URL', type=str, help='Enter the URL ')
     args = argParser.parse_args()
-    #print("Result : ", args.verbose,args.header,args.data,args.file,args.optional,args.URL)
+    if args.data and args.file:
+        print("Request format incorrect use one of -d or -f")
+        exit(1)
+
+    # print("Result : ", args.verbose,args.header,args.data,args.file,args.optional,args.URL)
     if args.req_type == 'get' and 'GET':
         if args.header == None:
-            nhead=" "
+            nhead = " "
             get(args.verbose, nhead, args.optional, args.URL)
-        else : get(args.verbose, args.header, args.optional, args.URL)
+        else:
+            get(args.verbose, args.header, args.optional, args.URL)
 
     elif args.req_type == 'post' and 'POST':
-        if args.header == None :
-            nhead=" "
+        if args.header == None:
+            nhead = " "
             post(args.verbose, nhead, args.data, args.file, args.optional, args.URL)
-        else : post(args.verbose, args.header, args.data, args.file, args.optional, args.URL)
-
-
+        else:
+            post(args.verbose, args.header, args.data, args.file, args.optional, args.URL)
 
 
 main()
-
-
